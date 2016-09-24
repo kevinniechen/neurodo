a8ba3357e0cf8ca0969cc3e453fb1e843d75ff8b
steven <stevengriffin323@gmail.com>
2016-09-24 03:32:55 -0400

    added ability to return more info from corpusdict

--- corpusdict.py
+++ corpusdict.py
@@ -5,35 +5,44 @@ from gensim import corpora, models, similarities
 from six import iteritems
 import re
 
-initializedItems = {}
+
+class GenSimBasics(object):
+    def __init__(self):
+        self.dictionary = None
+        self.corpus = None
+        self.words = None
 
 class MyCorpus(object):
     def __init__(self, dictionary, words):
         self.dictionary = dictionary
+        self.words = words
     def __iter__(self):
-        for sentence in words:
+        for sentence in self.words:
             # assume there's one document per line, tokens separated by whitespace
             yield self.dictionary.doc2bow(sentence)
 
-def compute_corpus_and_dict():
-    if initializedItems.get("corpus") and initializedItems.get("dict"):
-        return initializedItems["corpus"], initializedItems["dict"]
+initializedItems = GenSimBasics()
+
+def compute_basics():
+    if initializedItems.corpus:
+        return initializedItems
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-    # collect statistics about all tokens
+    # convert file to list of sentences, where sentences are split into words
     file = open('timecube_raw.txt')
     text = re.sub(r"""[^\w\s\.;!\?]""", '', file.read().lower())
     sentences = re.split(r'[\.!\?][ \n]', text)
-
-
     words = [sentence.split() for sentence in sentences]
-    print(words)
+
     dictionary = corpora.Dictionary(sentence for sentence in words)
     corpus = MyCorpus(dictionary, words)
 
+    initializedItems.words = words
+    initializedItems.corpus = corpus
+    initializedItems.dictionary = dictionary
+    return initializedItems
 
-
-    initializedItems["corpus"] = corpus
-    initializedItems["dict"] = dictionary
-    return corpus, dictionary
+def compute_corpus_and_dict():
+    items = compute_basics()
+    return items.corpus, items.dictionary
--- delusion_index/delusion_index.py
+++ delusion_index/delusion_index.py
@@ -3,10 +3,12 @@ from collections import defaultdict
 from pprint import pprint  # pretty-printer
 from gensim import corpora, models, similarities
 from six import iteritems
-from corpusdict import compute_corpus_and_dict
+from corpusdict import compute_basics
 
-corpus, dictionary = compute_corpus_and_dict()
-#lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
+basics = compute_basics()
+dictionary = basics.dictionary
+corpus = basics.corpus
+lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=len(basics.words))
 doc = "Human computer interaction"
 vec_bow = dictionary.doc2bow(doc.lower().split())
 #vec_lsi = lsi[vec_bow] # convert the query to LSI space
