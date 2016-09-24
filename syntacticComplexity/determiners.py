4aaa9347e3b5b9d46643151e35bad8a3ab12a6db
willshyr <williamshyr@Williams-MacBook-Pro.local>
2016-09-24 01:54:55 -0400

    determiners updated

--- syntacticComplexity/determiners.py
+++ syntacticComplexity/determiners.py
@@ -7,7 +7,26 @@ Created on Fri Sep 23 22:49:44 2016
 
 import nltk, re, pprint
 
+from collections import Counter
+from nltk import word_tokenize
+from urllib import request
 
 def determiners(filename):
-    
-    
+    f = open(filename, 'r')
+    raw = f.read()
+    # tokenization
+    tokens = word_tokenize(raw)
+    # print(tokens[1:10])
+    text = nltk.Text(tokens)
+    # print(text[1:10])
+    # print(type(tokens))
+    # print(type(text))
+    tagged = nltk.pos_tag(text)
+    counts = Counter(tag for word, tag in tagged)
+    # print(counts)
+    return counts["DT"]
+
+if __name__ == "__main__":
+    filename = "/Users/williamshyr/Documents/MedHacks/cohere_proj/timecube_raw.txt"
+    c = determiners(filename)
+    print(c)
