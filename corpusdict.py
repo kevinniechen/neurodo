77cb08ecb4f2bb9c9b3d1c924adfb4c76b59af64
steven <stevengriffin323@gmail.com>
2016-09-24 11:47:06 -0400

    iterative filter

--- corpusdict.py
+++ corpusdict.py
@@ -27,15 +27,20 @@ def compute_basics(file_name):
     text = ''
     with open(file_name) as file:
         text = file.read()
-    return compute_basics_text(text, lambda x: True)
+    return compute_basics_text(text, "all")
 
-def compute_basics_text(text, filter_func):
+def compute_basics_text(text, filter_even_odd):
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     # convert file to list of sentences, where sentences are split into words
     text = re.sub(r"""[^\w\s\.;!\?]""", '', text.lower())
     sentences = re.split(r'[\.!\?][ \n]', text)
-    sentences = filter(filter_func, sentences)
+
+    if filter_even_odd == 'even':
+        sentences = sentences[::2]
+    elif filter_even_odd == 'odd':
+        sentences = sentences[1::2]
+
     words = [sentence.split() for sentence in sentences]
 
     dictionary = corpora.Dictionary(sentence for sentence in words)
