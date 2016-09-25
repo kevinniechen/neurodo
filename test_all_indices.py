eb36dcd0092721a243df8e30e206028237ce4db5
steven <stevengriffin323@gmail.com>
2016-09-25 01:18:37 -0400

    results dict

--- test_all_indices.py
+++ test_all_indices.py
@@ -19,7 +19,7 @@ default_delusion_categories = [
 
 def delusion_index_file(file_name, concept_dict):
     basics = compute_basics(file_name)
-    return index_input(basics, concept_dict)
+    return index_input(basics, concept_dict), basics
 
 def delusion_index_string(text, concept_dict):
     basics = compute_basics_text(text, 'all')
@@ -30,27 +30,43 @@ def get_det_file(file_name):
         return get_num_determiners(f.read())
     return 0
 
-def test_dir(dir_name, concept_dict):
+def test_dir(dir_name, concept_dict, results_dict):
     for file_name in glob.glob(dir_name + '/*.txt'):
         print(file_name)
         sims = semantic_coherence.index_file(file_name)
-        delu = delusion_index_file(file_name, concept_dict)
-        num_det = get_det_file(file_name)
+        delu, basics = delusion_index_file(file_name, concept_dict)
+        num_det = get_det_file(file_name) / float(len(basics.words))
         max_phrase_len = maxPhraseLength(file_name)
-        #pprint(delu)
+        pprint(delu)
+
+
+        results_dict[file_name] = [
+                min(sims),
+                num_det,
+                max_phrase_len,
+                delu[default_delusion_categories[0]],
+                delu[default_delusion_categories[1]],
+                delu[default_delusion_categories[2]],
+                delu[default_delusion_categories[3]],
+                delu[default_delusion_categories[4]],
+                delu[default_delusion_categories[5]],
+        ]
         #print(sum(delu.values()))
-        print("sims min: " + str(min(sims)))
+        #print("sims min: " + str(min(sims)))
         #print("delu max: " + str(max(delu.values())))
         #print("num_det: " + str(num_det))
         #print("max_phrase_len: " + str(max_phrase_len))
-
+        #results_dict[file_name] = [min(sims),
+    return results_dict
 
 def run_all_tests():
     remove_delusions()
     add_delusions(default_delusion_categories)
     concept_dict = get_concepts_from_files()
 
-    test_dir('control_data', concept_dict)
-    test_dir('patient_data', concept_dict)
+    results_dict = {}
+    test_dir('control_data', concept_dict, results_dict)
+    test_dir('patient_data', concept_dict, results_dict)
+    return results_dict
 
 run_all_tests()
