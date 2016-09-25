66ecdb6ba01b34c1afc3c7b34891b325e7bee30c
steven <stevengriffin323@gmail.com>
2016-09-24 21:50:36 -0400

    work on testing

--- test_all_indices.py
+++ test_all_indices.py
@@ -5,6 +5,7 @@ from delusion_index.delusion_index import index_input
 from delusion_index.conceptnet5parser import get_concepts_from_files
 from delusion_index.delusion_queries import add_delusions, remove_delusions
 from corpusdict import compute_basics, compute_basics_text
+from syntacticComplexity.determiners import determiners
 
 default_delusion_categories = [
 'time',
@@ -23,11 +24,14 @@ def delusion_index_string(text, concept_dict):
     basics = compute_basics_text(text, 'all')
     return index_input(basics, concept_dict)
 
+
 remove_delusions()
 add_delusions(default_delusion_categories)
 concept_dict = get_concepts_from_files()
 
 for file_name in glob.glob('patient_data/*.txt'):
+    num_det = determiners(file_name)
+    pprint(num_det)
     sims_pat = semantic_coherence.index_file(file_name)
     delu_pat = delusion_index_file(file_name, concept_dict)
 
