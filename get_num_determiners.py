c904b5d748fa4532d49403170534fbc8c762230f
Kevin Chen <kevchen98@gmail.com>
2016-09-24 22:02:42 -0400

    Rewrite file

---
+++ get_num_determiners.py
@@ -0,0 +1,10 @@
+import re
+
+def get_num_determiners(text):
+    deter = ["that", "what", "whatever", "which", "whichever"]
+    count = 0
+
+    for word in deter:
+        count += sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(word), text))
+
+    return 0
